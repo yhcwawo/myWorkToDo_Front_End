7226230998b25ac1e29b6d5140f1422bfade1b41
yhcwawo <ghdcks1018@gmail.com>
2022-03-06 23:13:07 +0900

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,9 +3,9 @@ export const SERVICE_NAME = 'My work to do';
 export const TOKEN = 'TOKEN';
 export const USER = 'USER';
 export const developer = 'yhc';
-export const SERVICE_ID = 'service_your_worktodo';
-export const TEMPLATE_ID = 'template_p9hsprq';
-export const USER_ID = "user_bFe2ixCOQs1xG1uGe8IDf";
+export const SERVICE_ID = '';
+export const TEMPLATE_ID = '';
+export const USER_ID = '';
 
 
 
